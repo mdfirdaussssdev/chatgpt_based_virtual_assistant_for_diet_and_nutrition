d35e330b8dac05a82ddbe1b76e7d937458c57a5f
mdfirdaussssdev <mdfirdaussss27developer@gmail.com>
2024-10-21 00:27:13 +0800

    fix user non details view
    initially it will not return an error dialog if selectedactivitylevel is not empty.
    
    after test found out issue.

--- lib/views/user_no_user_details_view.dart
+++ lib/views/user_no_user_details_view.dart
@@ -138,7 +138,8 @@ class _UserNoUserDetailsViewState extends State<UserNoUserDetailsView> {
               _weight.text.isEmpty ||
               _height.text.isEmpty ||
               _selectedGoal == null ||
-              _selectedGender == null) {
+              _selectedGender == null ||
+              _selectedActivityLevel == null) {
             throw EmptyFieldViewException('All fields must be filled');
           }
           await _userDetailsService.createNewUserDetails(
